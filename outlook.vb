a44ba3221f81fa6143c3076c82b637988ce597c2
Trance <124764765+tranceuser@users.noreply.github.com>
2023-02-27 22:18:15 -0500

    update outlook.vb
    Replaced the Set objRegEx = CreateObject("VBScript.RegExp") line with the Set regex = CreateObject("VBScript.RegExp") line. This will make the variable names consistent, as regex is used throughout the rest of the code.
    
    Removed the Global and MultiLine properties from the regular expression object, as they are not needed in this case.
    
    Removed the If Not IsNumeric(strRoomNumber) Then ... End If block and replace it with a check using the regex.test method. This will allow you to handle cases where the room number is not found in the subject line, as well as cases where it is found but is not numeric.

--- outlook.vb
+++ outlook.vb
@@ -1,42 +1,37 @@
 Sub ReplyWithRandomGreetingAndResolvedIssue()
+    Dim objItem As Object
+    Dim strGreeting As String
+    Dim strTimeOfDay As String
+    Dim strRoomNumber As String
+    Dim arrReplies As Variant
+    Dim intRandomIndex As Integer
+    Dim objReply As MailItem
+    Dim regex As Object
 
-	Dim objItem As Object
-	Dim strGreeting As String
-	Dim strTimeOfDay As String
-	Dim strRoomNumber As String
-	Dim arrReplies As Variant
-	Dim intRandomIndex As Integer
-	Dim objReply As MailItem
-	Dim regex As Object
+    Set regex = CreateObject("VBScript.RegExp")
+    regex.Pattern = "\b\d{4,}\b"
 
-	Set regex = CreateObject("VBScript.RegExp")
-	regex.Pattern = "\d+"
+    If Application.ActiveExplorer.Selection.Count = 1 Then
+        Set objItem = Application.ActiveExplorer.Selection(1)
+        If TypeOf objItem Is MailItem Then
+            strRoomNumber = ""
+            If regex.test(objItem.Subject) Then
+                strRoomNumber = regex.Execute(objItem.Subject)(0)
+            End If
+            If strRoomNumber = "" Then
+                strRoomNumber = "the mentioned stateroom"
+            End If
 
-	If Application.ActiveExplorer.Selection.Count = 1 Then
-		Set objItem = Application.ActiveExplorer.Selection(1)
-		If TypeOf objItem Is MailItem Then
-			Set objRegEx = CreateObject("VBScript.RegExp")
-			With objRegEx
-				.Global = True
-				.MultiLine = True
-				.Pattern = "\b\d{4,}\b"
-				strRoomNumber = .Execute(objItem.Subject)(0)
-			End With
+            Select Case Hour(Now)
+                Case Is < 12
+                    strTimeOfDay = "morning"
+                Case 12 To 17
+                    strTimeOfDay = "afternoon"
+                Case Else
+                    strTimeOfDay = "evening"
+            End Select
 
-			If Not IsNumeric(strRoomNumber) Then
-				strRoomNumber = "the mentioned stateroom" ' If the room number is not numeric, set strRoomNumber to a default value
-			End If
-
-			Select Case Hour(Now)
-				Case Is < 12
-					strTimeOfDay = "morning"
-				Case 12 To 17
-					strTimeOfDay = "noon"
-				Case Else
-					strTimeOfDay = "evening"
-			End Select	
-
-			strGreeting = "Good " & strTimeOfDay & ", " & vbCrLf & vbCrLf
+            strGreeting = "Good " & strTimeOfDay & ", " & vbCrLf & vbCrLf
 
             ' Define an array of reply messages
             arrReplies = Array(strGreeting & "We wanted to let you know that the issue with cabin " & strRoomNumber & " has been resolved. If you have any further concerns, please let us know." & vbCrLf & vbCrLf & "Best Regards, " & vbCrLf & "Technical Team", _
